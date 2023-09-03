43767e3ea2b3b901ad54a08706a4a924a530701a
Aster <aster@vers.site>
2023-09-03 13:36:09 +0000

    🔧 Fix highlight on type alias

--- src/main/kotlin/com/github/bytecodealliance/ide/highlight/WitHighlightVisitor.kt
+++ src/main/kotlin/com/github/bytecodealliance/ide/highlight/WitHighlightVisitor.kt
@@ -17,6 +17,7 @@ class WitHighlightVisitor : WitVisitor(), HighlightVisitor {
 
     override fun visitUseAlias(o: WitUseAlias) {
         highlight(o.identifier, SYM_TYPE)
+        o.aliasName?.let { highlight(it, SYM_TYPE) }
     }
 
 
@@ -80,11 +81,11 @@ class WitHighlightVisitor : WitVisitor(), HighlightVisitor {
 
     override fun visitTypeGeneric(o: WitTypeGeneric) {
         when (o.identifier.text) {
-            "_", "bool",
+            "_", "bool", "char",
             "u8", "u16", "u32", "u64",
             "s8", "s16", "s32", "s64",
             "f32", "f64", "float32", "float64",
-            "string",
+            "string"
             -> {
                 highlight(o.identifier, KEYWORD)
             }
